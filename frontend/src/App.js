2e839a142d9e086a29732fa7b6839ac7c8f3e19e
Amandeep Singh <96967202+ASA-HELPER@users.noreply.github.com>
2023-04-09 14:37:58 +0530

    Completed routes work in frontend part

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,14 +2,76 @@ import './App.css';
 import {BrowserRouter,Route,Routes} from 'react-router-dom';
 import Home from './components/home/Home';
 import Header from './components/header/Header';
+import Footer from './components/footer/Footer';
+import About from "./components/About/About";
+import Projects from "./components/Projects/Projects";
+import Contact from "./components/Contact/Contact";
+import Login from "./components/Login/Login";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getUser, loadUser } from "./actions/user";
+import AdminPanel from "./components/Admin/AdminPanel";
+import Project from "./components/Admin/Project";
+import Loader from "./components/Loader/Loader";
+import Experiences from './components/Admin/Experiences';
+import LeetcodePost from './components/Admin/LeetcodePost';
 
 function App() {
+  const dispatch = useDispatch();
+
+  const { isAuthenticated } = useSelector((state) => state.login);
+  const { loading, user } = useSelector((state) => state.user);
+
+  useEffect(() => {
+    dispatch(getUser());
+    dispatch(loadUser());
+  }, [dispatch]);
   return (
     <BrowserRouter>
-    <Header/>
-      <Routes>
-        <Route path='/' element={<Home/>}/>
-      </Routes>
+    {loading ? (
+        <Loader />
+      ) : (
+        <>
+          <Header />
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <Home
+                  leetcodepost={user.leetcodepost}
+                  experiences={user.experiences}
+                  skills={user.skills}
+                />
+              }
+            />
+            <Route path="/about" element={<About about={user.about} />} />
+            <Route
+              path="/projects"
+              element={<Projects projects={user.projects} />}
+            />
+            <Route path="/contact" element={<Contact />} />
+            <Route
+              path="/account"
+              element={isAuthenticated ? <AdminPanel /> : <Login />}
+            />
+            <Route
+              path="/admin/experience"
+              element={isAuthenticated ? <Experiences /> : <Login />}
+            />
+            <Route
+              path="/admin/leetcodepost"
+              element={isAuthenticated ? <LeetcodePost /> : <Login />}
+            />
+
+            <Route
+              path="/admin/project"
+              element={isAuthenticated ? <Project /> : <Login />}
+            />
+          </Routes>
+
+          <Footer />
+        </>
+      )}
     </BrowserRouter>
   );
 }
